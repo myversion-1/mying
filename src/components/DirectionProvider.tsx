2100d1220e5b09ec19b789b83b88fac856c23714
miying-1 <sharonshareer@gmail.com>
2025-12-25 16:48:36 +0800

    Add RTL layout support for Arabic

---
+++ src/components/DirectionProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+import { useLanguage } from "./language";
+
+export function DirectionProvider({ children }: { children: React.ReactNode }) {
+  const { lang } = useLanguage();
+  const isRTL = lang === "ar";
+
+  useEffect(() => {
+    document.documentElement.dir = isRTL ? "rtl" : "ltr";
+    document.documentElement.lang = lang;
+  }, [lang, isRTL]);
+
+  return <>{children}</>;
+}
+
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { LanguageProvider } from "./language";
+import { DirectionProvider } from "./DirectionProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <LanguageProvider>{children}</LanguageProvider>;
+  return (
+    <LanguageProvider>
+      <DirectionProvider>{children}</DirectionProvider>
+    </LanguageProvider>
+  );
 }
 
